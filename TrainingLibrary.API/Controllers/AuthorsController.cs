868481825922c8d878d29f045100378241407673
MKatadzic <MKatadzic@MKATADZICW10>
2020-03-10 09:55:47 +0100

    Add AuthorsController

---
+++ TrainingLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,25 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TrainingLibrary.API.Controllers
+{
+    [ApiController]
+    public class AuthorsController : ControllerBase // not form Controller -> we dont need Views 
+    {
+        private readonly ICourseLibraryRepository _course;
+
+        public AuthorsController(ICourseLibraryRepository course)
+        {
+            _course = course ??
+                throw new ArgumentNullException(nameof(course));
+        }
+
+        [HttpGet("api/authors")]
+        public IActionResult GetAuthors()
+        {
+            var authors = _course.GetAuthors();
+            return new JsonResult(authors);
+        }
+    }
+}
