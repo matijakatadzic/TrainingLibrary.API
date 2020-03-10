f8fd62c96b78ad235366433ccea6534b510aada5
MKatadzic <MKatadzic@MKATADZICW10>
2020-03-10 11:23:15 +0100

    Add Courses logic

---
+++ TrainingLibrary.API/Controllers/CoursesController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using TrainingLibrary.API.Models;
+
+namespace TrainingLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors/{authorId}/courses")]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _course;
+        private readonly IMapper _mapper;
+
+        public CoursesController(ICourseLibraryRepository course, IMapper mapper)
+        {
+            _course = course ??
+                throw new ArgumentNullException(nameof(course));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CourseDto>> GetCoursesForAuthor(Guid authorId)
+        {
+            if(!_course.AuthorExists(authorId: authorId))
+            {
+                return NotFound();
+            }
+
+            var courses = _course.GetCourses(authorId);
+
+            return Ok(_mapper.Map<IEnumerable<CourseDto>>(courses));
+        }
+    }
+}
---
+++ TrainingLibrary.API/Profiles/CoursesProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Entities;
+using TrainingLibrary.API.Helpers;
+using TrainingLibrary.API.Models;
+
+namespace TrainingLibrary.API.Profiles
+{
+    public class CoursesProfile : Profile
+    {
+        public CoursesProfile()
+        {
+            CreateMap<Course, CourseDto>();
+        }
+    }
+}
