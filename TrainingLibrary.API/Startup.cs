af9333d6e395b3c7d7b0cae4957fd1e37355a702
MKatadzic <MKatadzic@MKATADZICW10>
2020-03-10 09:14:02 +0100

    Add Data model & EFCore nuget

--- TrainingLibrary.API/Startup.cs
+++ TrainingLibrary.API/Startup.cs
@@ -1,10 +1,14 @@
+using CourseLibrary.API.DbContexts;
+using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace TrainingLibrary.API
+namespace CourseLibrary.API
 {
     public class Startup
     {
@@ -18,7 +22,15 @@ namespace TrainingLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+           services.AddControllers();
+             
+            services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
+
+            services.AddDbContext<CourseLibraryContext>(options =>
+            {
+                options.UseSqlServer(
+                    @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
+            }); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
